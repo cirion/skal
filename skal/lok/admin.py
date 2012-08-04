d4de02d6adf6e171fbaa0c6133cd5f2d76cc9261
Chris King <cirion@gmail.com>
2012-08-04 08:37:17 +0000

    Add a new 'Result' model that links a choice and the outcomes.

--- skal/lok/admin.py
+++ skal/lok/admin.py
@@ -1,12 +1,31 @@
-from lok.models import Scenario, Choice
+from lok.models import Scenario, Choice, MoneyOutcome, StatOutcome, Result
 from django.contrib import admin
+
+class MoneyOutcomeInline(admin.TabularInline):
+	model = MoneyOutcome
+
+class StatOutcomeInline(admin.TabularInline):
+	model = StatOutcome
+
+class ResultInline(admin.StackedInline):
+	model = Result
 
 class ChoiceInline(admin.StackedInline):
 	model = Choice
-	extra = 3
+	inlines = [ResultInline]
 
 class ScenarioAdmin(admin.ModelAdmin):
 	inlines = [ChoiceInline]
 	search_fields = ['title', 'description']
 
+class ChoiceAdmin(admin.ModelAdmin):
+	model = Choice
+	inlines = [ResultInline]
+
+class ResultAdmin(admin.ModelAdmin):
+	model = Result
+	inlines = [MoneyOutcomeInline, StatOutcomeInline]
+
 admin.site.register(Scenario, ScenarioAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Result, ResultAdmin)
--- skal/lok/models.py
+++ skal/lok/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
+STAT_HUNTING = 1
+STAT_STEALTH = 2
+STAT_SWORDFIGHTING = 3
+STAT_SAILING = 4
+STAT_NAVIGATION = 5
+STAT_CHOICES = (
+		(STAT_HUNTING, "Hunting"),
+		(STAT_STEALTH, "Stealth"),
+		(STAT_SWORDFIGHTING, "Swordfighting"),
+		(STAT_SAILING, "Sailing"),
+		(STAT_NAVIGATION, "Navigation")
+)
+
 class Scenario(models.Model):
 	title = models.CharField(max_length=100)
-	image = models.CharField(max_length=200)
+	image = models.ImageField(blank=True,null=True,upload_to="/home/chris/django/skal/pics")
 	description = models.TextField(max_length=2000)
 	TYPE_QUEST = 1
 	TYPE_ENCOUNTER = 2
@@ -21,17 +34,24 @@ class Choice(models.Model):
 	def __unicode__(self):
 		return self.title
 
-class MoneyOutcome(models.Model):
+class Result(models.Model):
 	choice = models.ForeignKey(Choice)
+	title = models.CharField(max_length=100)
+	description = models.TextField(max_length=4000)
+	def __unicode__(self):
+		return self.title
+
+class MoneyOutcome(models.Model):
+	choice = models.ForeignKey(Result)
 	amount = models.IntegerField()
 	def __unicode__(self):
-		return self.amount;
+		return self.amount
 	
 class StatOutcome(models.Model):
-	choice = models.ForeignKey(Choice)
-	stat = models.CharField(max_length=50)
+	choice = models.ForeignKey(Result)
+	stat = models.IntegerField(choices=STAT_CHOICES, default=STAT_SWORDFIGHTING)
 	amount = models.IntegerField()
 	def __unicode__(self):
-		return self.stat
+		return str(self.stat)
 
 
